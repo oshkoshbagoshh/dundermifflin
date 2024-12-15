fc2b11a2bd2c5f684639efaaaccbc5e34fa476f9
AJ Javadi <amirjavadi25@gmail.com>
2024-12-14 22:30:10 -0600

    Models and migrations set up in right order.

--- zzMISC/tags_categories_queries.php
+++ zzMISC/tags_categories_queries.php
@@ -1,3 +1,26 @@
 <?php
 
 
+// Get all parent categories with their children
+use App\Models\Category;
+use App\Models\Tag;
+use App\Models\Comment;
+
+$categories = Category::parents()->with('children')->get();
+
+// Get all posts in a category including subcategories
+$categoryPosts = Category::find($id)->posts()
+    ->orWhereIn('category_id', Category::where('parent_id', $id)->pluck('id'))
+    ->get();
+
+// Get popular tags
+$popularTags = Tag::popular()->limit(10)->get();
+
+// Get posts with specific tag
+$tagPosts = Tag::where('slug', $slug)->firstOrFail()->posts;
+
+// Add tags to a post
+$post->tags()->attach($tagIds);
+
+// Get posts with categories and tags
+$posts = Post::with(['category', 'tags'])->latest()->get();
